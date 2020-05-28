8b0ab3e37ff3b7f8e4307e4c7fb838795966c2fc
Michael Neely <michael@michaelneely.net>
2020-05-27 23:13:13 -0400

    Added the fs function to add new files

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const http = require('http');
-const data = {
-    firstName: 'Michael',
-    lastName: 'Neely'
-}
+const fs = require('fs');
 const site = http.createServer(function(req, res) {
-    console.log('hello World');
-    console.log(req.headers);
-    console.log(req.url);
-    res.setHeader('Content-Type','application/json');
-    res.write(JSON.stringify(data));
-    res.end();
+    fs.readFile('test.json', function(error, data) {
+        let holder = JSON.parse(data);
+        res.setHeader('Content-Type','application/json');
+        res.write(data);
+        console.log(holder.firstName)
+        res.end();
+    });
+    
 });
 
 site.listen(3000);
