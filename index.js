3367b99d384a31f6314bdb6f26ef773218e07b5b
Michael Neely <michael@michaelneely.net>
2020-05-27 23:36:14 -0400

    Added Noemon, Express, and my gitignore file to exclude node_modules

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const http = require('http');
-const fs = require('fs');
-const site = http.createServer(function(req, res) {
-    fs.readFile('test.json', function(error, data) {
-        let holder = JSON.parse(data);
-        res.setHeader('Content-Type','application/json');
-        res.write(data);
-        console.log(holder.firstName)
-        res.end();
-    });
+// const http = require('http');
+// const fs = require('fs');
+// console.log('RESTART');
+// const site = http.createServer(function(req, res) {
+//     fs.readFile('test.json', function(error, data) {
+//         let holder = JSON.parse(data);
+//         res.setHeader('Content-Type','application/json');
+//         res.write(data);
+//         console.log(holder.firstName)
+//         res.end();
+//     });
     
+// });
+
+// site.listen(3000);
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('<h1>Sup Homeslice!</h1>');
 });
 
-site.listen(3000);
+app.listen(3000);
